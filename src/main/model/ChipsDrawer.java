8e81708e27924d61a214d29a86d18ba263bb8aec
aria231 <aria231@students.cs.ubc.ca>
2021-02-23 10:33:11 -0800

    Added more functions to the game

--- src/main/model/ChipsDrawer.java
+++ src/main/model/ChipsDrawer.java
@@ -58,7 +58,7 @@ public class ChipsDrawer {
 
     //REQUIRES: drawer is not empty and drawer contains chip
     //MODIFIES: this
-    //EFFECTS: removes chip from drawer
+    //EFFECTS: removes the chip from drawer
     public void removeChipFromDrawer(Chips chip) {
         String colour = chip.getColour();
         for (int i = 0; i < drawer.size(); i++) {
